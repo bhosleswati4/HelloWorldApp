788eb42a9881f4459e945aaf776a25ee8ab1196c
bholeswati4 <bholeswati4@gmail.com>
2020-06-07 10:46:45 -0400

    First commit

---
+++ src/main/java/com/trail/com/App.java
@@ -0,0 +1,13 @@
+package com.trail.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
